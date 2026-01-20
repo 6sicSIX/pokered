6c490b12a12641402c704aaf96fa7e3671ff344c
6sicSIX <killiankane@outlook.com>
2026-01-20 08:09:08 +0000

    Merge Red/Blue wild encounters
    Removes version exclusives so both appear in one ROM

--- data/wild/maps/CeruleanCave1F.asm
+++ data/wild/maps/CeruleanCave1F.asm
@@ -1,20 +1,15 @@
 CeruleanCave1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 46, GOLBAT
-	db 46, HYPNO
-	db 46, MAGNETON
-	db 49, DODRIO
-	db 49, VENOMOTH
-IF DEF(_RED)
-	db 52, ARBOK
-ENDC
-IF DEF(_BLUE)
-	db 52, SANDSLASH
-ENDC
-	db 49, KADABRA
-	db 52, PARASECT
-	db 53, RAICHU
 	db 53, DITTO
+	db 49, VENOMOTH
+	db 49, DODRIO
+	db 52, SANDSLASH
+	db 46, GOLBAT
+	db 53, RAICHU
+	db 46, HYPNO
+	db 52, PARASECT
+	db 49, KADABRA
+	db 46, MAGNETON
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
--- data/wild/maps/PowerPlant.asm
+++ data/wild/maps/PowerPlant.asm
@@ -1,21 +1,15 @@
 PowerPlantWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 21, VOLTORB
-	db 21, MAGNEMITE
-	db 20, PIKACHU
-	db 24, PIKACHU
-	db 23, MAGNEMITE
-	db 23, VOLTORB
-	db 32, MAGNETON
-	db 35, MAGNETON
-IF DEF(_RED)
-	db 33, ELECTABUZZ
 	db 36, ELECTABUZZ
-ENDC
-IF DEF(_BLUE)
-	db 33, RAICHU
+	db 23, VOLTORB
+	db 21, MAGNEMITE
+	db 21, VOLTORB
+	db 23, MAGNEMITE
 	db 36, RAICHU
-ENDC
+	db 32, MAGNETON
+	db 24, PIKACHU
+	db 35, MAGNETON
+	db 20, PIKACHU
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
